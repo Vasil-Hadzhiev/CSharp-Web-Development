b36f4e352f1239763bfd3016ac4997b0ce6d0266
Vasil <vsl.h.94@gmail.com>
2018-05-27 15:32:23 +0300

    Completed lab

---
+++ Web Development Basics/03.Web Server - Asynchronous Processing/Lab/01.EvenNumbersThread/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.EvenNumbersThread
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var start = tokens[0];
+            var end = tokens[1];
+
+            var thread = new Thread(() => PrintEvenNumbers(start, end));
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("Thread finished work");
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Web Development Basics/03.Web Server - Asynchronous Processing/Lab/03.SimpleWebServer/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace _03.SimpleWebServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var address = IPAddress.Parse("127.0.0.1");
+            var port = 1300;
+            var server = new TcpListener(address, port);
+            server.Start();
+
+            Console.WriteLine("Server started.");
+            Console.WriteLine($"Listening to TCP clients at 127.0.0.1:{port}");
+
+            var task = Task.Run(async () => await Connect(server));
+            task.Wait();
+        }
+
+        private static async Task Connect(TcpListener listener)
+        {
+            while (true)
+            {
+                //wait for browser to connect
+                Console.WriteLine("Waiting for client...");
+                var client = await listener.AcceptTcpClientAsync();
+
+                Console.WriteLine("Client connected");
+
+                //read request
+                var request = new byte[1024];
+                await client.GetStream().ReadAsync(request, 0, request.Length);
+
+                var message = Encoding.UTF8.GetString(request);
+                Console.WriteLine(message);
+
+                //send request
+                var data = Encoding.UTF8.GetBytes("Hello from server!");
+                await client.GetStream().WriteAsync(data, 0, data.Length);
+                client.Dispose();
+            }
+        }
+    }
+}
